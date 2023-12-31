0d5a41c88cc8c366316b6613eefad4cb5f906297
linux_user <lucasherediadv@gmail.com>
2023-12-31 11:35:34 -0300

    adding more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,32 @@
 """Docs"""
 import unittest
-from src import main
+from src.main import roll_a_dice, execute_roll_a_dice, find_matches
 
 
 class TestMain(unittest.TestCase):
+
+
     """Docs"""
     def test_roll_a_dice(self):
         """Docs"""
-        result = main.roll_a_dice()
+        result = roll_a_dice()
         self.assertEqual(len(result), 5)
         self.assertTrue(all(1 <= num <= 6 for num in result))
+
+
+    def test_execute_roll_a_dice(self):
+        """Docs"""
+        result = execute_roll_a_dice()
+        self.assertEqual(len(result), 6)
+        self.assertTrue(all(isinstance(num_str, str) for num_str in result))
+
+
+    def test_find_matches(self):
+        """Docs"""
+        word_list = [['11111', 'first'],['22222', 'second'],['33333', 'third']]
+        all_results = ['11111','22222','44444','55555','66666']
+        result = find_matches(word_list, all_results)
+        self.assertEqual(result, 'first second')
 
 
 if __name__ == '__main__':
