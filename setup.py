d9e973801169faf5719a85d4fbc2a71b0533e57b
linux_user <lucasherediadv@gmail.com>
2023-12-31 14:27:10 -0300

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 """Docs"""
 from setuptools import setup
 
-setup()
+setup(
+    name="diceware",
+    version="1.0.0",
+    description="Simple Diceware Passphrase Generator",
+    author="Lucas Heredia",
+    # author_email="",
+    # url="",
+    packages=["src"],
+    entry_points={
+        "console_scripts": ["diceware = src.main:main"],
+    },
+)
