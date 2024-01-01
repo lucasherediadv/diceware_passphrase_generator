15c842e3be2638ff923f8d388bbc8ea765b24dc4
linux_user <lucasherediadv@gmail.com>
2024-01-01 20:35:00 -0300

    adding package setup

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
-"""Docs"""
+"""Initialize the setup process"""
+
 from setuptools import setup
 
-setup(
-    name="diceware",
-    version="1.0.0",
-    description="Simple Diceware Passphrase Generator",
-    author="Lucas Heredia",
-    # author_email="",
-    # url="",
-    packages=["src"],
-    entry_points={
-        "console_scripts": ["diceware = src.main:main"],
-    },
-)
+setup()
